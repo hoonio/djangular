42e0746fb4058f2c8fb2ba74a7b4d4f090dd88de
Hoonio <hoonio@hoonio.com>
2016-04-01 03:19:15 +0100

    azure host settings

--- djangular/settings.py
+++ djangular/settings.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -84,7 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
